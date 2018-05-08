f1d14849a79da9e78ec3ed6011c893e9d3b684a9
root <root@Flako-PC.localdomain>
2018-05-08 15:59:19 -0400

    Adding testing framework and cleaning code

--- test/test.js
+++ test/test.js
@@ -1,18 +1,32 @@
 const assert = require('chai').assert;
-const algo = require("../MergeSort").mergeSort;
+var mergeSort = require("../MergeSort").mergeSort;
+//var fromArray = require('stream-array');
+//var concat = require('concat-stream');
+//var pump = require('pump');
 //mergeSortResult = mergeSort([1,3,2,4,5]);
 
 describe("Algo", function () {
-  it('Hello', function () {
-    assert.equal(algo([1, 3, 2, 4, 5]), ([1, 2, 3, 4, 5]));
-  });
-
+  var arr =[ 1, 3, 2, 4, 5];
+  new mergeSort(arr);
+    
 
   it('app should return', function () {
-    assert.equal(mergeSort([1, 3, 2, 4, 5]), ([1, 2, 3, 4, 5]));
+    expect(result).to.eql([1,2,3,4,5]);
   });
 
   it("mergeSort should return an array of ints ", function () {
     assert.typeOf(algo([1, 3, 2, 4, 5]), 'number');
   });
 });
+
+function euclidean(a, b) {
+	return Math.sqrt(b.x*b.x+b.y*b.y) - Math.sqrt(a.x*a.x+a.y*a.y);
+}
+
+function increasing(a, b) {
+	return b-a;
+}
+
+function toArray(callback) {
+	return concat({encoding: 'object'}, callback);
+}
