ecb162cf13a7e2d2fe3123c14a7c11d10b5dedec
root <root@Flako-PC.localdomain>
2018-05-08 15:33:20 -0400

    Adding testing framework and cleaning code

--- MergeSort.js
+++ MergeSort.js
@@ -1,36 +1,33 @@
 // @ts-check
-function mergeSort(arr) {
-    if (arr.length === 1) {
-        return arr;
+module.export = function () {
+    function mergeSort(arr) {
+        if (arr.length === 1) {
+            return arr;
+        }
+        var mergeArr = arr;
+        const middle = Math.floor(mergeArr.length / 2);
+        const left = mergeArr.slice(0, middle);
+        const right = mergeArr.slice(middle, mergeArr.length);
+        return merge(mergeSort(left), mergeSort(right));
     }
-    var mergeArr = arr;
-
-    const middle = Math.floor(mergeArr.length / 2);
-    const left = mergeArr.slice(0, middle);
-    const right = mergeArr.slice(middle, mergeArr.length);
-
-    return merge(mergeSort(left), mergeSort(right));
-}
-function merge(left, right) {
-    var sortedArr = [];
-
-    while (left.length && right.length) {
-        if (left[0] <= right[0]) {
+    function merge(left, right) {
+        var sortedArr = [];
+        while (left.length && right.length) {
+            if (left[0] <= right[0]) {
+                sortedArr.push(left.shift());
+            }
+            else {
+                sortedArr.push(right.shift());
+            }
+        }
+        while (left.length) {
             sortedArr.push(left.shift());
-        } else {
+        }
+        while (right.length) {
             sortedArr.push(right.shift());
         }
+        return sortedArr;
     }
-    while (left.length) {
-        sortedArr.push(left.shift());
-    }
-    while (right.length) {
-        sortedArr.push(right.shift());
-    }
-
-    return sortedArr;
-}
-
 
 var arrNums = [10, 7, 3, 1, 9, 8, 4, 2];
 var arrChars = ['e', 'c', 'b', 'a', 'f', 'h', 'g', 'd'];
@@ -44,8 +41,11 @@ console.log('Array Strings :', arrStrings.join(' '));
 console.log('Array mixed   :', arrCharsMixed.join(' '));
 
 console.log("-------------RESULTS-----------------");
+
+console.log('Array Nums    :', mergeSort([1,3,2,4,5]).join(" "));
 console.log('Array Nums    :', mergeSort(arrNums).join(" "));
 
 console.log('Array Chars   :', mergeSort(arrChars).join(" "));
 console.log('Array Strings :', mergeSort(arrStrings).join(" "));
 console.log('Array mixed   :', mergeSort(arrCharsMixed).join(" "));
+}
--- test/test.js
+++ test/test.js
@@ -1,5 +1,18 @@
-it('double done', function(done) {
-    // Calling `done()` twice is an error
-    setImmediate(done);
-    setImmediate(done);
+const assert = require('chai').assert;
+const algo = require("../MergeSort").mergeSort;
+//mergeSortResult = mergeSort([1,3,2,4,5]);
+
+describe("Algo", function () {
+  it('Hello', function () {
+    assert.equal(algo([1, 3, 2, 4, 5]), ([1, 2, 3, 4, 5]));
   });
+
+
+  it('app should return', function () {
+    assert.equal(mergeSort([1, 3, 2, 4, 5]), ([1, 2, 3, 4, 5]));
+  });
+
+  it("mergeSort should return an array of ints ", function () {
+    assert.typeOf(algo([1, 3, 2, 4, 5]), 'number');
+  });
+});
